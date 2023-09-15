16301100fc6ad83cdf65ff83afcc76c61b404363
abrenzink <a.brenzink@gmail.com>
2023-09-15 17:36:35 -0300

    corrections

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongodb = require('./data/database');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
